01e8c94a71bd2af396aa37c026868dcee52dd464
Davi Meireles <45656609+davimeireles@users.noreply.github.com>
2024-12-14 21:48:38 +0000

    DAVI 14/12/2024 21:47 New struct for the project, database must be created for each project inside src folder for microservices.

--- Docker/backend/src/manage.py
+++
@@ -1,4 +0,0 @@
-# Este é um utilitário de linha de comando que permite interagir com o projeto Django de várias maneiras:
-# como iniciar um servidor de desenvolvimento
-# aplicar migrações de banco de dados
-# criar aplicativos, etc.
--- Docker/backend/src/transcendence/__init__.py
+++
@@ -1,2 +0,0 @@
-# Este arquivo vazio indica que o diretório myproject deve ser tratado como um pacote Python.
-# Ele é necessário para que o Python reconheça o diretório como um módulo.
