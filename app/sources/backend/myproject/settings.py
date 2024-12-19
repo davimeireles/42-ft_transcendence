817f839ee3fedfe4d1887f72ee4ce30accaaf4d1
Davi <davimeirelespn@gmail.com>
2024-12-19 16:50:12 +0000

    DAVI 19/12/2024 16:50 change content for file html

--- app/sources/backend/myproject/settings.py
+++ app/sources/backend/myproject/settings.py
@@ -66,11 +66,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': os.environ.get('POSTGRES_PORT'),
+        'NAME': 'postgres',
+        'USER': 'admin',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
