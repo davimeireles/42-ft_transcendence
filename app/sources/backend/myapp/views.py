1397d2a7c7253d244924ea64a275deab2897ad28
Davi <davimeirelespn@gmail.com>
2024-12-20 23:23:56 +0000

    DAVI 20/12/2024 23:22 database tables created in models.py small change in index.html trying to use a button to test models class.

--- app/sources/backend/myapp/views.py
+++ app/sources/backend/myapp/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from models import User
 
-def index(request):
-    return HttpResponse(models.User.objects.all())
+# Create views
