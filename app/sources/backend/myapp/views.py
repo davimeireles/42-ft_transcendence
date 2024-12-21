2e1abfa9cb86371530cbc1d37f47dd08f42807be
Davi Meireles <45656609+davimeireles@users.noreply.github.com>
2024-12-21 22:21:47 +0000

    Davi 21/12/2024 22:21 restframework working to see database modules default values for game table and gametype table serializer created for each module

--- app/sources/backend/myapp/views.py
+++ app/sources/backend/myapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Games, GameType, Tournament, Match, MatchParticipant, User
+from rest_framework import viewsets
+from .serializers import UserSerializer, GamesSerializer, GameTypeSerializer, TournamentSerializer, MatchSerializer, MatchParticipantSerializer
 
 # Create views
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class GamesViewSet(viewsets.ModelViewSet):
+    queryset = Games.objects.all()
+    serializer_class = GamesSerializer
+
+class GameTypeViewSet(viewsets.ModelViewSet):
+    queryset = GameType.objects.all()
+    serializer_class = GameTypeSerializer
+
+class TournamentViewSet(viewsets.ModelViewSet):
+    queryset = Tournament.objects.all()
+    serializer_class = TournamentSerializer
