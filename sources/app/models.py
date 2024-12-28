c9ab1b908d05331f235d011146f1ebe880e90b0e
Davi <davimeirelespn@gmail.com>
2024-12-28 10:49:54 +0000

    Davi 28/12/2024 10:47 Big changes: New folder struct Django using frontend template to work based on then Database working normally Nginx working with mkcert Nginx loading files from frontend folder
    TODO: Work with 42 authentication by now.

---
+++ sources/app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=32, null=False, unique=True)
+    password = models.CharField(max_length=128, null=False)
+    nickname = models.CharField(max_length=32, null=False, unique=True)
+    email = models.CharField(max_length=128, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+class Games(models.Model):
+    name = models.CharField(max_length=32, null=False, unique=True)
+
+class GameType(models.Model):
+    type = models.CharField(max_length=32, null=False, unique=True)
+    
+class Tournament(models.Model):
+    winner = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+class Match(models.Model):
+    gameID = Games.id
+    userID = User.id
+    gameTypeID = GameType.id
+    tournamentID = Tournament.id
+
+class MatchParticipant(models.Model):
+    matchID = Match.id
+    userID = User.id
+    score = models.IntegerField(null=True)
