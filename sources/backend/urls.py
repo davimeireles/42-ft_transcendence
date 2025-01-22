bf524791c09d3489bbdae5acce8e37da1fc369f5
Davi Nunes <davimeirelespn@gmail.com>
2025-01-22 12:20:45 +0000

    Davi 22/01/2025 12:20

--- sources/backend/urls.py
+++ sources/backend/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from app import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.register_user, name='register_user'),
     path('login/', views.login_user, name='login_user'),
-]
+    path('auth-42/', views.redirect_42, name='redirect_42'),
+    path('new/', views.new, name='new'),
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
