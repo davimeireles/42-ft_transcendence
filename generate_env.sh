5d525f5a6ce88824cbaf5ab65cde72a1bff67fe9
Davi Nunes <davimeirelespn@gmail.com>
2025-01-27 16:56:56 +0000

    Function to inset SendGridAPIKEY when gerenate envs

--- generate_env.sh
+++ generate_env.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Prompt the user for the SendGrid API key if not set
+if [ -z "$SENDGRID_API_KEY" ]; then
+  read -sp 'Enter your SendGrid API Key: ' SENDGRID_API_KEY
+  echo
+fi
+
 # Define o conteúdo do arquivo .env
 env_content="
 POSTGRES_DB=postgres
@@ -12,7 +18,7 @@ PGADMIN_PASSWORD=admin
 UID=u-s4t2ud-74d8db07ab23dbb9f83dacd063f1e8b2fb38bd85da84522873b98d6342dd18ec
 SECRET=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
 URI=http://127.0.0.1:8000/new/
-SENDGRID_API_KEY=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
+SENDGRID_API_KEY=$SENDGRID_API_KEY
 DEFAULT_EMAIL_SENDER=davimeirelespn@gmail"
 
 # Cria o arquivo .env e escreve o conteúdo nele
