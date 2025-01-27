ea91df8aaebddff7a34a8cd0e4511e02a9e8352c
Davi Nunes <davimeirelespn@gmail.com>
2025-01-27 13:42:41 +0000

    Merge

--- generate_env.sh
+++ generate_env.sh
@@ -13,9 +13,7 @@ UID=u-s4t2ud-74d8db07ab23dbb9f83dacd063f1e8b2fb38bd85da84522873b98d6342dd18ec
 SECRET=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
 URI=http://127.0.0.1:8000/new/
 SENDGRID_API_KEY=\$SENDGRID_API_KEY
-DEFAULT_EMAIL_SENDER=davimeirelespn@gmail.com
-"
-
+DEFAULT_EMAIL_SENDER=davimeirelespn@gmail
 # Cria o arquivo .env e escreve o conteúdo nele
 echo "$env_content" > .env
 
