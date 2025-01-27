0b9a694be7e5f9e30d30d440417a2e28e8ba46cf
Davi Nunes <davimeirelespn@gmail.com>
2025-01-27 16:46:43 +0000

    Changed generate_envs file

--- generate_env.sh
+++ generate_env.sh
@@ -12,8 +12,9 @@ PGADMIN_PASSWORD=admin
 UID=u-s4t2ud-74d8db07ab23dbb9f83dacd063f1e8b2fb38bd85da84522873b98d6342dd18ec
 SECRET=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
 URI=http://127.0.0.1:8000/new/
-SENDGRID_API_KEY=\$SENDGRID_API_KEY
-DEFAULT_EMAIL_SENDER=davimeirelespn@gmail
+SENDGRID_API_KEY=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
+DEFAULT_EMAIL_SENDER=davimeirelespn@gmail"
+
 # Cria o arquivo .env e escreve o conteúdo nele
 echo "$env_content" > .env
 
